d395d8ca04c36ee4927fbabea173d695fd2dab2e
Praveen S <praveenselvaraj2004@gmail.com>
2026-06-23 01:16:56 +0530

    modified code

--- js/main.js
+++ js/main.js
@@ -1,33 +1,5 @@
 const HomebooksContainer = document.querySelector(".books_container")
 
-// function createBookCard(book) {
-//     return `
-//         <article class="book-card" data-id="${book.id}">
-//             <img src="${book.image}" alt="${book.title}" loading="lazy">
-//             <h3>${book.title}</h3>
-//             <p class="author">${book.author}</p>
-//             <p class="price">
-//                     ₹${book.price}
-//                     <small><s>₹${book.originalPrice}</s></small>
-//             </p>
-//             <button
-//                 class="add-to-cart"
-//                 data-id="${book.id}"
-//                 ${!book.inStock ? "disabled" : ""}
-//             >
-//                 ${book.inStock ? "Add to Cart" : "Out of Stock"}
-//             </button>
-//         </article>
-//     `
-// }
-
-// function renderBook(bookList) {
-//     if (!HomebooksContainer) return;
-
-//     const allHTML = bookList.map(book => createBookCard(book)).join("")
-
-//     HomebooksContainer.innerHTML = allHTML
-// }
 
 function attachAddToCartListeners() {
 
